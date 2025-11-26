e848163fe8d61ba23d9f27858d387f8af35dbaf6
farhanakbar2605-code <farhanakbar2605@gmail.com>
2025-11-26 15:37:47 +0700

    Add files via upload

---
+++ P10/bangundatar.py
@@ -0,0 +1,22 @@
+def persegi(sisi):
+    return sisi * sisi
+
+print(persegi(10))
+
+def persegi_panjang(panjang,lebar):
+    hasil = panjang * lebar
+    return hasil
+
+print(persegi_panjang(12,5))
+
+def segitiga(alas,tinggi):
+    hasil = 0.5 * alas * tinggi
+    return hasil
+
+print(segitiga(9,7))
+
+def jajargenjang(alas,tinggi):
+    hasil = alas * tinggi
+    return hasil
+
+print(jajargenjang(5,8))
---
+++ P10/bangunruang.py
@@ -0,0 +1,30 @@
+def kubus (sisi):
+    hasil = sisi**3
+    return hasil
+
+
+def balok(p,l,t):
+    hasil = p * l * t
+    return hasil
+
+
+def prisma(alas, tinggi_segitiga, tinggi_prisma):
+    luas_alas = 0.5 * alas * tinggi_segitiga
+    hasil = luas_alas * tinggi_prisma
+    return hasil
+
+
+
+def tabung(r,t):
+     hasil = 3.14*r**2*t
+     return hasil
+
+
+
+def kerucut(r,t):
+    hasil = 1/3*3.14*r**2*t
+    return hasil
+
+
+
+
